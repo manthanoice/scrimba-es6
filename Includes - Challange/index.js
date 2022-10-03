f72c4dc5f5d3dc955ba0abc72efc7639b2e18c10
Manthanoice <mvbosamiya20@gmail.com>
2022-10-03 15:57:45 +0530

    😔

---
+++ Includes - Challange/index.js
@@ -0,0 +1,6 @@
+const listIngredients =  [ "flour", "sugar", "eggs", "butter" ];
+
+if (listIngredients.includes("chocolate"))
+    console.log("We're going to make chocolate cake")
+else
+    console.log("Sed")
